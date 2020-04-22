83e9fccceaf34922bc193ac46c1862b6dca0b71f
Spudymun <spudymun@gmail.com>
2020-04-22 13:13:21 +0300

    Updates

---
+++ precache-manifest.1dd53b7669d2797807be95a0fe102152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc8a6d70cef7a923c4456a3ed9be3773",
+    "url": "/Spudymun/test/index.html"
+  },
+  {
+    "revision": "61169f2a16a409b22f8b",
+    "url": "/Spudymun/test/static/css/main.6880f69e.chunk.css"
+  },
+  {
+    "revision": "0a593331dbd451cb8270",
+    "url": "/Spudymun/test/static/js/2.b077063e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Spudymun/test/static/js/2.b077063e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61169f2a16a409b22f8b",
+    "url": "/Spudymun/test/static/js/main.450cd4a5.chunk.js"
+  },
+  {
+    "revision": "9d0a87d6009afc15629c",
+    "url": "/Spudymun/test/static/js/runtime-main.21713a03.js"
+  }
+]);
